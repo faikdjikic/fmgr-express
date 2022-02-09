0bff45f177cbb7c4202b77d8f93fb81f27ad6187
Faik Djikic <faik@djikic.com>
2022-02-09 15:52:06 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const router = express.Router();
+const config = require('./lib/config');
+const path = require("path");
+const process = require("process");
+const appDir = process.cwd();
+const rootDirArr = config.fmRootDir.split("/");
+const rootDir = path.join(appDir, ...rootDirArr);
+const multer = require("multer");
+const upload = multer({ dest: "uploads" });
+const uploadFile = require('./lib/uploadFile');
+const makeDirectory = require('./lib/makeDir');
+const listDirectory = require('./lib/listDir');
+const searchDirectory = require('./lib/searchDir');
+if (config.maxsize) {
+  let unitPow = 2;
+  if (isNaN(config.maxsize)) {
+    let maxSizeUnit = String(config.maxsize).substring(String(config.maxsize).length - 1).toUpperCase();
+    let maxSizeValue = Number(String(config.maxsize).substring(0, String(config.maxsize).length - 1));
+    if (maxSizeUnit == 'K') {
+      unitPow = 1;
+    } else if (maxSizeUnit == 'G') {
+      unitPow = 3;
+    }
+    config.allowSize = maxSizeValue * Math.pow(1024, unitPow)
+  } else {
+    config.allowSize = config.maxsize * Math.pow(1024, unitPow);
+  }
+} else {
+  config.allowSize = 2 * Math.pow(1024, 2);
+}
+/* Upload file or make directory. */
+router.post('*', upload.single('uFile'), async function (req, res, next) {
+  if (req.file) {
+    const retVal = await uploadFile(req, config, rootDir);
+    res.json(retVal);
+  } else if (req.body.dirName) {
+    const retVal = await makeDirectory(req, config, rootDir);
+    res.json(retVal);
+  } else {
+    res.json({ ErrorCode: 403, ErrorDesc: `No file received! You did not send valid multipart form data.`, Success: false });
+    return;
+  }
+
+});
+/* List directory or search files by pattern */
+router.get('*', async function (req, res, next) {
+  let getDirList;
+  if (req.query.q) {
+    getDirList = await searchDirectory(req, next, config, rootDir);
+  } else {
+    getDirList = await listDirectory(req, next, config, rootDir);
+  }
+  if (getDirList.ErrorCode == 0) {
+    res.render(path.join(__dirname, '.', 'views', 'index'), getDirList.data);
+  } else {
+    getDirList.baseUrl=config.baseUrl;
+    res.render(path.join(__dirname, '.', 'views', 'error'), getDirList);
+  }
+});
+
+module.exports = router;
